50c0407d7fe9495937c8a0619a387d93582a22ad
hayde <hayde@Hayder.attlocal.net>
2025-01-29 20:12:32 -0600

    java is fun

--- src/demo1/DemoTest3.java
+++ src/demo1/DemoTest3.java
@@ -3,6 +3,14 @@ package demo1;
 public class DemoTest3 {
 	public static void main(String args []) {
 		System.out.println("Java");
+		
+		int a =10;
+		int b=40;
+		int result = b+a;
+		System.out.println(result);
+		
+		
+		
 	}
 
 }
